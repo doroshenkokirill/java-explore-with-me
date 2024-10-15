c53cb0e955600100ffdbd8a3ed12e3dfc124bb8c
Kirill <148490432+doroshenkokirill@users.noreply.github.com>
2024-10-15 16:26:58 +0300

    add: exceptions + fix: HitService

--- stat/service/src/main/java/ru/practicum/repository/HitRepository.java
+++ stat/service/src/main/java/ru/practicum/repository/HitRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.dto.HitStatDto;
 import ru.practicum.model.Hit;
 
@@ -11,24 +10,34 @@ import java.util.List;
 
 public interface HitRepository extends JpaRepository<Hit, Integer> {
 
-    @Query("SELECT new ru.practicum.dto.HitStatDto(app, uri, COUNT(ip)) " +
-            "FROM Hit " +
-            "WHERE timestamp BETWEEN :start AND :end " +
-            "AND uri IN (:uris) " +
-            "GROUP BY app, uri ")
-    List<HitStatDto> findAllHits(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end,
-            @Param("uris") List<String> uris);
+    @Query("SELECT new ru.practicum.dto.HitStatDto(h.app, h.uri, COUNT (DISTINCT h.ip)) " +
+            "FROM Hit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT (DISTINCT h.ip) DESC ")
+    List<HitStatDto> findAllUniqueHitsWhenUriIsEmpty(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.dto.HitStatDto(hit.app, hit.uri, COUNT(DISTINCT hit.ip)) " +
-            "FROM Hit AS hit " +
-            "WHERE hit.timestamp BETWEEN :start AND :end " +
-            "AND (:uris IS NULL OR hit.uri IN :uris) " +
-            "GROUP BY hit.app, hit.uri " +
-            "ORDER BY COUNT(DISTINCT hit.ip) DESC")
-    List<HitStatDto> findUniqueHits(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end,
-            @Param("uris") List<String> uris);
+    @Query("SELECT new ru.practicum.dto.HitStatDto(h.app, h.uri, COUNT (DISTINCT h.ip)) "
+            + "FROM Hit h "
+            + "WHERE h.timestamp BETWEEN :start AND :end "
+            + "AND h.uri IN (:uris)"
+            + "GROUP BY h.app, h.uri "
+            + "ORDER BY COUNT (DISTINCT h.ip) DESC ")
+    List<HitStatDto> findAllUniqueHitsWhenUriIsNotEmpty(
+            LocalDateTime start, LocalDateTime end, List<String> uris);
+
+    @Query("SELECT new ru.practicum.dto.HitStatDto(h.app, h.uri, COUNT (h.ip))" +
+            "FROM Hit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT (h.ip) DESC ")
+    List<HitStatDto> findAllHitsWhenUriIsEmpty(LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT new ru.practicum.dto.HitStatDto(h.app, h.uri, count (h.ip)) "
+            + "FROM Hit h "
+            + "WHERE h.timestamp BETWEEN :start AND :end "
+            + "AND h.uri in (:uris)"
+            + "GROUP BY h.app, h.uri "
+            + "ORDER BY COUNT (h.ip) DESC ")
+    List<HitStatDto> findAllHitsWhenStarEndUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
