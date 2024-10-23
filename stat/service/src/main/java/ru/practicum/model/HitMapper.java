050642fc3e9f2bfc5caa354ebafe8595dcf09f24
Kirill <doroshenkokirill56@yandex.ru>
2024-10-23 06:53:33 +0300

    fix: rollback somefixes

--- stat/service/src/main/java/ru/practicum/model/HitMapper.java
+++ stat/service/src/main/java/ru/practicum/model/HitMapper.java
@@ -3,16 +3,21 @@ package ru.practicum.model;
 import lombok.experimental.UtilityClass;
 import ru.practicum.dto.HitDto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @UtilityClass
 public class HitMapper {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static Hit toHit(HitDto hitDto) {
+        LocalDateTime localDateTime = LocalDateTime.parse(hitDto.getTimestamp(), formatter);
         return Hit.builder()
                 .id(hitDto.getId())
                 .uri(hitDto.getUri())
                 .app(hitDto.getApp())
                 .ip(hitDto.getIp())
-                .timestamp(hitDto.getTimestamp())
+                .timestamp(localDateTime)
                 .build();
     }
 }
