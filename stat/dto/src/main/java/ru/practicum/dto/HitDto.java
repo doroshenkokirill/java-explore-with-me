91d658815190840ee6cabad3ac4941c7a4e3bfae
Kirill <doroshenkokirill56@yandex.ru>
2024-10-24 22:16:55 +0300

    fix: rollback all changes

--- stat/dto/src/main/java/ru/practicum/dto/HitDto.java
+++ stat/dto/src/main/java/ru/practicum/dto/HitDto.java
@@ -1,11 +1,8 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -15,6 +12,5 @@ public class HitDto {
     private String app;
     private String uri;
     private String ip;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timestamp;
+    private String timestamp;
 }
