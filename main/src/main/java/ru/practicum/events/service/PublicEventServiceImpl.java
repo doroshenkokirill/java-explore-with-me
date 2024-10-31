0bac17022b008c0d1ae16a0bfa0644d27a72925f
Kirill <148490432+doroshenkokirill@users.noreply.github.com>
2024-10-31 14:59:31 +0300

    add: PublicEventServiceImpl

--- main/src/main/java/ru/practicum/events/service/PublicEventServiceImpl.java
+++ main/src/main/java/ru/practicum/events/service/PublicEventServiceImpl.java
@@ -1,27 +1,52 @@
 package ru.practicum.events.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import ru.practicum.events.dto.EventFullDto;
 import ru.practicum.events.dto.EventShortDto;
+import ru.practicum.events.model.Event;
+import ru.practicum.events.model.EventMapper;
+import ru.practicum.events.model.enums.EventState;
 import ru.practicum.events.repository.EventRepository;
+import ru.practicum.exeptions.NotFoundException;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PublicEventServiceImpl implements PublicEventService {
     private final EventRepository eventRepository;
 
     @Override
-    public List<EventShortDto> getEventsList(String text, Set<Integer> categories, Boolean paid, LocalDateTime rangeStart, LocalDateTime rangeEnd, Boolean onlyAvailable, String sort, int from, int size) {
+    public List<EventShortDto> getEventsList(String text, Set<Integer> categories, Boolean paid,
+                                             LocalDateTime rangeStart, LocalDateTime rangeEnd,
+                                             Boolean onlyAvailable, String sort, int from, int size) {
         return List.of();
     }
 
     @Override
     public EventFullDto get(int id) {
-        return null;
+        checkId(id, eventRepository);
+        Event event = eventRepository.findById(id).get();
+
+        if (!event.getState().equals(EventState.PUBLISHED)) {
+            throw new NotFoundException(String.format("There are no published events with id %d", id));
+        }
+        //TODO
+        EventFullDto result = EventMapper.toEventFullDto(eventRepository.save(event));
+        log.info("Result of get event by Id {}: {}", id, result);
+        return result;
+    }
+
+    private void checkId(int id, JpaRepository<?, Integer> repository) {
+        if (!repository.existsById(id)) {
+            log.info("Not found with id: {}", id);
+            throw new NotFoundException("Not found with id " + id + " from: " + repository);
+        }
     }
 }
