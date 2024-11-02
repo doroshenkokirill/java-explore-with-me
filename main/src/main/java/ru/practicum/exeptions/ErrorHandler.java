245c1266d194cb8938eb629ff6fe2b3aa380493e
Kirill <doroshenkokirill56@yandex.ru>
2024-11-02 19:21:08 +0300

    fix: ErrorResponse

--- main/src/main/java/ru/practicum/exeptions/ErrorHandler.java
+++ main/src/main/java/ru/practicum/exeptions/ErrorHandler.java
@@ -11,54 +11,53 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
-    //TODO rework error response
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse errorBadRequest(final BadRequestException e) {
         log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Incorrectly made request.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse errorNotFound(final NotFoundException e) {
         log.debug("Получен статус 404 Not found {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.NOT_FOUND, "The required object was not found.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse errorNotUnique(final NotUniqueException e) {
         log.debug("Получен статус 409 Not Unique {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.CONFLICT, "Integrity constraint has been violated.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse errorThrowable(final Throwable e) {
         log.debug("Получен статус 500 Internal error {}. ", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected server error.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse errorConflict(final ConflictException e) {
         log.debug("Получен статус 409 Conflict {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.CONFLICT, "Integrity constraint has been violated.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse errorMethodArgumentNotValid(final MethodArgumentNotValidException e) {
         log.debug("Получен статус 400 Argument not valid {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Invalid request parameters.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse errorMissingServletRequestParameter(final MissingServletRequestParameterException e) {
         log.debug("Получен статус 400 Missing Servlet request parameter {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, "Missing required request parameter.", e.getMessage());
     }
 }
--- main/src/main/java/ru/practicum/exeptions/ErrorResponse.java
+++ main/src/main/java/ru/practicum/exeptions/ErrorResponse.java
@@ -1,12 +1,24 @@
 package ru.practicum.exeptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class ErrorResponse {
-    private final String error;
+    private final String status;
+    private final String reason;
+    private final String message;
 
-    public ErrorResponse(String error) {
-        this.error = error;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime timestamp;
+
+    public ErrorResponse(HttpStatus status, String reason, String message) {
+        this.status = status.name();
+        this.reason = reason;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
     }
 }
