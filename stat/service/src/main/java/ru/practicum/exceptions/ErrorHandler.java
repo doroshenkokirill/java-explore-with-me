4726100e74ae2e7943bf55986d605fe552b277d3
Kirill <doroshenkokirill56@yandex.ru>
2024-11-03 15:31:56 +0300

    fix: ErrorHandler

--- stat/service/src/main/java/ru/practicum/exceptions/ErrorHandler.java
+++ stat/service/src/main/java/ru/practicum/exceptions/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +22,13 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse errorInternal(final Exception e) {
         log.debug("Получен статус 500 Internal error {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse errorMissingServletRequestParameter(final MissingServletRequestParameterException e) {
+        log.debug("Получен статус 400 Missing Servlet request parameter {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 }
